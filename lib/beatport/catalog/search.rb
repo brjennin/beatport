82401876aa17c8dd2108176a0933890a02c23b0e
Brian Jennings <brian.j.jennings@gmail.com>
2012-05-12 10:28:33 -0700

    Make it so you can determine what is returned by the search

--- lib/beatport/catalog/search.rb
+++ lib/beatport/catalog/search.rb
@@ -1,11 +1,18 @@
 module Beatport
-  module Catalog  
+  module Catalog
     class Search
       def self.query(query, options = {})
         options['query'] = query
-      
+
         Client.retrieve 'search', :auto, options
       end
+
+      def self.query_of_type(query, type, options = {})
+        options['query'] = query
+
+        Client.retrieve 'search', type, options
+      end
+
     end
   end
 end
