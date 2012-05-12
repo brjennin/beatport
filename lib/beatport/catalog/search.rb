01e6eec4713bbe5667751c63c5e9f003fe09752f
Brian Jennings <brian.j.jennings@gmail.com>
2012-05-12 10:55:47 -0700

    trying to get this to work

--- lib/beatport/catalog/search.rb
+++ lib/beatport/catalog/search.rb
@@ -10,7 +10,7 @@ module Beatport
       def self.query_of_type(query, type, options = {})
         options['query'] = query
 
-        Client.retrieve 'search', type.constantize, options
+        Client.retrieve 'search', Beatport::Catalog.send(type.constantize), options
       end
 
     end
